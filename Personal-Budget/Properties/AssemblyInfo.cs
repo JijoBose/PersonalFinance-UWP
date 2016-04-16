3885b8e27d1a6820973b84187e697c3ab2e4a0c2
Jijo Bose <jijobose@msn.com>
2016-04-16 21:04:08 +0530

    Updated License

--- Personal-Budget/Properties/AssemblyInfo.cs
+++ Personal-Budget/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Personal_Budget")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("App to manage your personal expenses")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Personal_Budget")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Jijo Bose")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
