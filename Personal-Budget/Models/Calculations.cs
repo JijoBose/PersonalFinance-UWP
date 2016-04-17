7e8668c97c474885a6474cb1b116e0c32f17eaeb
Jijo Bose <jijobose@msn.com>
2016-04-17 08:15:18 +0530

    Changes to file

--- Personal-Budget/Models/Calculations.cs
+++ Personal-Budget/Models/Calculations.cs
@@ -1,18 +1,21 @@
-﻿//Personal Finance App to manage your income and expenses.
-//Copyright(C) 2016  Jijo Bose
+﻿// ************************************************************************
+//Personal Finance - App to track your income, expenses and assets.
+//Copyright (C) 2016  Jijo Bose
 
 //This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU Affero General Public License as published
-//by the Free Software Foundation, either version 3 of the License, or
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
 //(at your option) any later version.
 
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//GNU Affero General Public License for more details.
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
 
-//You should have received a copy of the GNU Affero General Public License
-//along with this program.If not, see<http://www.gnu.org/licenses/>.
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **************************************************************************
+
 
 using System;
 using System.Collections.Generic;
